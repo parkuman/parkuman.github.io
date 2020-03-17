ab60c6c37ff5e819c22b5ec30a2447ad9d6f7230
Parkuman <parkumania@gmail.com>
2020-03-17 19:43:32 -0400

    Updates

---
+++ precache-manifest.ea8d66e419d3a0dc022814389a915ea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a26c37682524bb3f2734a8c2657819",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0466db289c672319f90c",
+    "url": "/static/css/main.8065129b.chunk.css"
+  },
+  {
+    "revision": "c11e4b22e5713af4d64a",
+    "url": "/static/js/2.b84250a3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b84250a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0466db289c672319f90c",
+    "url": "/static/js/main.76db7f7c.chunk.js"
+  },
+  {
+    "revision": "92a8fa6d2a61009e0c1c",
+    "url": "/static/js/runtime-main.e3441cc1.js"
+  }
+]);
